afe2f925ea32b945ef8efce6bf4f7e6c561c138b
gamoreno <Mrgabo0107>
2023-08-07 23:14:54 +0200

    do only mains

--- cpp05/ex03/includes/Intern.hpp
+++ cpp05/ex03/includes/Intern.hpp
@@ -1,22 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamoreno <gamoreno@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 21:51:36 by gamoreno          #+#    #+#             */
+/*   Updated: 2023/08/07 21:59:30 by gamoreno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef INTERN_HPP
 # define INTERN_HPP
 
 # include <iostream>
 # include <string>
+# include "AForm.hpp"
+# include "ShrubberyCreationForm.hpp"
+# include "RobotomyRequestForm.hpp"
+# include "PresidentialPardonForm.hpp"
+
 
 class Intern
 {
-
 	public:
-
 		Intern();
-		Intern( Intern const & src );
+		Intern(const Intern &src);
 		~Intern();
 
-		Intern &		operator=( Intern const & rhs );
-
+		Intern &operator=(Intern const &rhs);
+		AForm	*makeForm(std::string, std::string);
 };
-
-std::ostream &			operator<<( std::ostream & o, Intern const & i );
-
 #endif /* ********************************************************** INTERN_H */
--- cpp05/ex03/sources/Intern.cpp
+++ cpp05/ex03/sources/Intern.cpp
@@ -1,55 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Intern.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamoreno <gamoreno@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 22:01:07 by gamoreno          #+#    #+#             */
+/*   Updated: 2023/08/07 22:15:21 by gamoreno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Intern.hpp"
 
 /*
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
-
 Intern::Intern()
-{
-}
+{}
 
-Intern::Intern( const Intern & src )
+Intern::Intern(const Intern &src)
 {
+	*this = src;
 }
-
 
 /*
 ** -------------------------------- DESTRUCTOR --------------------------------
 */
-
 Intern::~Intern()
-{
-}
-
+{}
 
 /*
 ** --------------------------------- OVERLOAD ---------------------------------
 */
-
-Intern &				Intern::operator=( Intern const & rhs )
+Intern &Intern::operator=(Intern const &rhs)
 {
-	//if ( this != &rhs )
-	//{
-		//this->_value = rhs.getValue();
-	//}
+	(void)rhc; 
 	return *this;
 }
-
-std::ostream &			operator<<( std::ostream & o, Intern const & i )
-{
-	//o << "Value = " << i.getValue();
-	return o;
-}
-
 
 /*
 ** --------------------------------- METHODS ----------------------------------
 */
+AForm	*Intern::makeForm(std::string form, std::string target)
+{
+	int i;
+	std::string	forms[3] = {"Shrubbery" , "Presidential", "Robotomy"};
 
-
-/*
-** --------------------------------- ACCESSOR ---------------------------------
-*/
-
+	for (i = 0; i < 3; i++)
+	{
+		if (form == forms[i])
+			break ;
+	}
+	switch (i)
+	{
+	case 0:
+		std::cout << "Intern creates shrubbery creation form" << std::endl;
+		return (new ShrubberyCreationForm(target));
+		break;
+	case 1:
+		std::cout << "Intern creates presidential pardon form" << std::endl;
+		return (new PresidentialPardonForm(target));
+		break;
+	case 2:
+		std::cout << "Intern creates robotomy request form" << std::endl;
+		return (new RobotomyRequestForm(target));
+		break;
+	default:
+		std::cout << "Intern cannot find form." << std::endl;
+		return (nullptr);
+		break;
+	}
+}
 
 /* ************************************************************************** */
