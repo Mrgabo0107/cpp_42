cd93ec7174c69aadc95bd0f776ca0e5facb01414
gamoreno <Mrgabo0107>
2023-08-19 14:45:28 +0200

    adding some test to last exercise

--- cpp07/ex02/sources/main.cpp
+++ cpp07/ex02/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gamoreno <gamoreno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 21:34:03 by gamoreno          #+#    #+#             */
-/*   Updated: 2023/08/18 22:09:02 by gamoreno         ###   ########.fr       */
+/*   Updated: 2023/08/19 14:43:27 by gamoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,60 @@
 
 
 int main() {
-    Array<int> intArray(5);
-    for (size_t i = 0; i < intArray.size(); ++i) {
-        intArray[i] = i * 2;
-        std::cout << intArray[i] << " ";
+	//Testing empty array
+	Array<int> emptyArray;
+	std::cout << "Size of empty array: " << emptyArray.size() << std::endl;
+		
+	//testing with an int array
+	Array<int> intArray(5);
+	for (size_t i = 0; i < intArray.size(); ++i) {
+		intArray[i] = i * 2;
+		std::cout << intArray[i] << " ";
     }
-    std::cout << std::endl;
+	std::cout << std::endl;
 
-    try {
-        int outOfBounds = intArray[10];
-        (void)outOfBounds;
-    } catch (const std::exception& e) {
-        std::cerr << "Exception caught: " << e.what() << std::endl;
-    }
+	try {
+		int outOfBounds = intArray[10];
+		(void)outOfBounds;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
+	
+	//testing copy constructor
+	Array<int> intCopy(intArray);
+	for (size_t i = 0; i < intArray.size(); ++i)
+		std::cout << intArray[i] << " ";
+	std::cout << std::endl;
 
-    return 0;
+	//ANother type	
+	Array<char> charArray(10);
+	for (size_t i = 0; i < charArray.size(); ++i) {
+		charArray[i] = (i + 33);
+		std::cout << "\'" << charArray[i] << "\' ";
+	}
+	std::cout << std::endl;
+	
+	try {
+		char outOfBounds = charArray[11];
+		(void)outOfBounds;
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+	}
+
+	Array< Array<int> > intArrayArray(4);
+	Array<int> auxIntArray(4);
+	
+	for (size_t i = 0; i < intArrayArray.size(); ++i) {
+		intArrayArray[i] = auxIntArray;
+		for (size_t j = 0; j < auxIntArray.size(); ++j) {
+			intArrayArray[i][j] = i + j;
+			std::cout << intArrayArray[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+	
+	return 0;
 }
