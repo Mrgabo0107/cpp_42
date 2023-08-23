ea62134dd1fa4b998f85720b673e6ac3ede7b002
Gabriel Moreno <gdmorenor@unal.edu.co>
2023-08-23 02:40:20 +0200

    doing better 08/ex02

--- cpp08/ex02/sources/main.cpp
+++ cpp08/ex02/sources/main.cpp
@@ -6,70 +6,41 @@
 /*   By: gamoreno <gamoreno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 21:37:37 by gamoreno          #+#    #+#             */
-/*   Updated: 2023/08/22 22:11:02 by gamoreno         ###   ########.fr       */
+/*   Updated: 2023/08/23 02:37:58 by gamoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <stack>
-// #include <iterator>
 
 template <typename T>
-class MutantStack : public std::stack<T>
-{
+class MutantStack : public std::stack<T> {
 public:
-    class iterator : public std::iterator<std::bidirectional_iterator_tag, T>
-    {
-    private:
-        typename std::stack<T>::container_type::iterator current;
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-    public:
-        iterator(typename std::stack<T>::container_type::iterator it) : current(it) {}
-
-        bool operator==(const iterator &other) const { return current == other.current; }
-        bool operator!=(const iterator &other) const { return !(*this == other); }
-
-        iterator &operator++()
-        {
-            ++current;
-            return *this;
-        }
-
-        iterator &operator--()
-        {
-            --current;
-            return *this;
-        }
-
-        T &operator*() { return *current; }
-    };
-
-    typedef typename std::stack<T>::container_type::iterator container_iterator;
-
-    iterator begin() { return iterator(std::stack<T>::c.begin()); }
-    iterator end() { return iterator(std::stack<T>::c.end()); }
+    iterator begin() { return this->c.begin(); }
+    const_iterator begin() const { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    const_iterator end() const { return this->c.end(); }
 };
 
-int main()
-{
+int main() {
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
+
     std::cout << mstack.top() << std::endl;
     mstack.pop();
     std::cout << mstack.size() << std::endl;
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
-    mstack.push(0);
 
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite)
-    {
+
+    while (it != ite) {
         std::cout << *it << std::endl;
         ++it;
     }
@@ -77,4 +48,3 @@ int main()
     std::stack<int> s(mstack);
     return 0;
 }
-
